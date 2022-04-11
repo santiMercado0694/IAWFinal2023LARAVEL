cb5d37adc539dbba991e63aa62a23f8e10d1674d
santiMercado0694 <santimercado06_94@hotmail.com>
2022-04-11 11:20:55 -0300

    Agregue Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+Use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $Data = [
+           [
+            'name' => 'Santiago' ,
+            'email' => 'santimercado06.94@gmail.com' ,
+            'email_verified_at' => null ,
+            'password' => bcrypt('12341234') ,
+           ],
+           [ 
+            'name' => 'Juan' ,
+            'email' => 'Juancito0305@gmail.com' ,
+            'email_verified_at' => null ,
+            'password' => bcrypt('12341234') ,
+           ],
+           [
+            'name' => 'Raul' ,
+            'email' => 'raulguarnizo@gmail.com' ,
+            'email_verified_at' => null ,
+            'password' => bcrypt('12341234') ,
+           ],
+        ];
+        DB::table('users')->insert($Data);
     }
 }
