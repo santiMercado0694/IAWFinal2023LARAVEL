eb62ec26db7a687f2dc94ebf10c79abc50d8ded4
santiMercado0694 <santimercado06_94@hotmail.com>
2022-05-01 21:18:02 -0300

    cambios para testear crud

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -15,7 +15,7 @@
                             {{ method_field('PATCH') }}
                             @csrf
 
-                            @include('product.form')
+                            
 
                         </form>
                                                 
